92671d368dfe334b37a3ecb90c32e5f02b1cbcd6
Pietro Celeste <p.celeste@osynapsy.net>
2022-08-26 19:18:02 +0200

    Added license disclaimer

--- src/Osynapsy/Bcl4/Calendar/Calendar.php
+++ src/Osynapsy/Bcl4/Calendar/Calendar.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the Osynapsy package.
+ *
+ * (c) Pietro Celeste <p.celeste@osynapsy.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Osynapsy\Bcl4\Calendar;
 
 use Osynapsy\Html\Component;
@@ -9,7 +19,7 @@ use Osynapsy\Bcl4\Button;
 /**
  * Description of Calendar
  *
- * @author Peter
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
  */
 class Calendar extends Component
 {
