7f7cb5b80b4b12d09e815b83aea531530db9e433
Pietro Celeste <p.celeste@osynapsy.net>
2022-08-27 09:44:02 +0200

    Added license disclaimer

--- src/Osynapsy/Bcl4/Calendar/Strategy/MontlyStrategy.php
+++ src/Osynapsy/Bcl4/Calendar/Strategy/MontlyStrategy.php
@@ -1,16 +1,20 @@
 <?php
-namespace Osynapsy\Bcl4\Calendar\Strategy;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This file is part of the Osynapsy package.
+ *
+ * (c) Pietro Celeste <p.celeste@osynapsy.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
+
+namespace Osynapsy\Bcl4\Calendar\Strategy;
 
 /**
  * Description of MontlyStrategy
  *
- * @author pietr
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
  */
 class MontlyStrategy
 {
